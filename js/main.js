b68a299d051430bee98a82e3ddfecda6a2c1ef81
mostafawaleed3 <mostafawaleed206@gmail.com>
2022-06-18 14:18:04 +0200

    development

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
-const slider = document.querySelector('.slider');
-const boxes = document.querySelectorAll('.slider li');
-const indicators = document.querySelectorAll('.indicators li button');
-const wrapper = document.querySelector('.wrapper');
+const slider = document.querySelector(".slider");
+const boxes = document.querySelectorAll(".slider > *");
+const indicators = document.querySelectorAll(".indicators li button");
+const wrapper = document.querySelector(".wrapper");
 
-
-wrapper.classList.add('open');
+wrapper.classList.add("open");
 const boxesLength = boxes.length;
-slider.style.width = boxesLength * 100 + '%';
+slider.style.width = boxesLength * 100 + "%";
 
 indicators.forEach((indicator, i) => {
-  indicator.addEventListener('click', () => {
+  indicator.addEventListener("click", () => {
     const activeIndicator = document.querySelector(
-      '.indicators li button.active'
+      ".indicators li button[aria-current='true']"
     );
-
-    activeIndicator.setAttribute('aria-current', false);
-    activeIndicator.classList.remove('active');
-
-    indicator.setAttribute('aria-current', true);
-    indicator.classList.add('active');
+    activeIndicator.setAttribute("aria-current", false);
+    indicator.setAttribute("aria-current", true);
 
     slider.style.transform =
-      'translateX(' + i * (-(1 / boxesLength) * 100) + '%)';
+      "translateX(" + i * (-(1 / boxesLength) * 100) + "%)";
     // console.log(i);
   });
 });
